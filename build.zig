6e5c71bf001801d07203a9d887dc98f172087a02
Trace Andreason <tandreason@gmail.com>
2024-05-02 15:05:58 -0700

    cleaner build

--- build.zig
+++ build.zig
@@ -8,44 +8,39 @@ pub fn build(b: *Build) !void {
     const target = b.standardTargetOptions(.{});
     const optimize = b.standardOptimizeOption(.{});
 
-    const dep_sokol = b.dependency("sokol", .{
-        .target = target,
-        .optimize = optimize,
-    });
-
-    const hello = b.addExecutable(.{
-        .name = "hello",
-        .target = target,
-        .optimize = optimize,
-        .root_source_file = b.path("src/hello.zig"),
-    });
-    hello.root_module.addImport("sokol", dep_sokol.module("sokol"));
-    b.installArtifact(hello);
-
-    const triangle = b.addExecutable(.{
+    const examples: [5]exampleBuildConfig = .{ .{
+        .name = "in-out",
+        .path = "src/1-5-shaders/1-in-out.zig",
+    }, .{
+        .name = "uniforms",
+        .path = "src/1-5-shaders/2-uniforms.zig",
+    }, .{
+        .name = "attributes",
+        .path = "src/1-5-shaders/3-attributes.zig",
+    }, .{
         .name = "triangle",
-        .target = target,
-        .optimize = optimize,
-        .root_source_file = b.path("src/triangle.zig"),
-    });
-    triangle.root_module.addImport("sokol", dep_sokol.module("sokol"));
-    b.installArtifact(triangle);
-    const runo = b.addRunArtifact(triangle);
-    b.step("run-triangle", "run triangle").dependOn(&runo.step);
-    const run = b.addRunArtifact(hello);
-    b.step("run", "Run hello").dependOn(&run.step);
+        .path = "src/triangle.zig",
+    }, .{
+        .name = "hello",
+        .path = "src/hello.zig",
+    } };
 
-    buildExample(b, "src/1-5-shaders/1-in-out.zig", "in-out", target, optimize);
-    buildExample(b, "src/1-5-shaders/2-uniforms.zig", "uniforms", target, optimize);
-    buildExample(b, "src/1-5-shaders/3-attributes.zig", "attributes", target, optimize);
+    inline for (examples) |example| {
+        buildExample(b, example, target, optimize);
+    }
 }
 
-fn buildExample(b: *Build, path: []const u8, comptime name: []const u8, target: ResolvedTarget, optimize: OptimizeMode) void {
+const exampleBuildConfig = struct {
+    name: []const u8,
+    path: []const u8,
+};
+
+fn buildExample(b: *Build, comptime conf: exampleBuildConfig, target: ResolvedTarget, optimize: OptimizeMode) void {
     const triangle = b.addExecutable(.{
-        .name = name,
+        .name = conf.name,
         .target = target,
         .optimize = optimize,
-        .root_source_file = b.path(path),
+        .root_source_file = b.path(conf.path),
     });
     const dep_sokol = b.dependency("sokol", .{
         .target = target,
@@ -54,5 +49,5 @@ fn buildExample(b: *Build, path: []const u8, comptime name: []const u8, target: 
     triangle.root_module.addImport("sokol", dep_sokol.module("sokol"));
     b.installArtifact(triangle);
     const runo = b.addRunArtifact(triangle);
-    b.step("run-" ++ name, "run " ++ name).dependOn(&runo.step);
+    b.step("run-" ++ conf.name, "run " ++ conf.name).dependOn(&runo.step);
 }
