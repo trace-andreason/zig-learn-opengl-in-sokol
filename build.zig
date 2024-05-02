1c1a44222560ddc9e1d8178ce96ae2bcb7556900
Trace Andreason <tandreason@gmail.com>
2024-05-02 14:39:28 -0700

    x

--- build.zig
+++ build.zig
@@ -2,6 +2,7 @@ const std = @import("std");
 
 const Build = std.Build;
 const OptimizeMode = std.builtin.OptimizeMode;
+const ResolvedTarget = std.Build.ResolvedTarget;
 
 pub fn build(b: *Build) !void {
     const target = b.standardTargetOptions(.{});
@@ -33,4 +34,25 @@ pub fn build(b: *Build) !void {
     b.step("run-triangle", "run triangle").dependOn(&runo.step);
     const run = b.addRunArtifact(hello);
     b.step("run", "Run hello").dependOn(&run.step);
+
+    buildExample(b, "src/1-5-shaders/1-in-out.zig", "in-out", target, optimize);
+    buildExample(b, "src/1-5-shaders/2-uniforms.zig", "uniforms", target, optimize);
+    buildExample(b, "src/1-5-shaders/3-attributes.zig", "attributes", target, optimize);
+}
+
+fn buildExample(b: *Build, path: []const u8, comptime name: []const u8, target: ResolvedTarget, optimize: OptimizeMode) void {
+    const triangle = b.addExecutable(.{
+        .name = name,
+        .target = target,
+        .optimize = optimize,
+        .root_source_file = b.path(path),
+    });
+    const dep_sokol = b.dependency("sokol", .{
+        .target = target,
+        .optimize = optimize,
+    });
+    triangle.root_module.addImport("sokol", dep_sokol.module("sokol"));
+    b.installArtifact(triangle);
+    const runo = b.addRunArtifact(triangle);
+    b.step("run-" ++ name, "run " ++ name).dependOn(&runo.step);
 }
---
+++ src/1-5-shaders/3-attributes.zig
@@ -0,0 +1,75 @@
+const sokol = @import("sokol");
+const slog = sokol.log;
+const sg = sokol.gfx;
+const sapp = sokol.app;
+const sglue = sokol.glue;
+const stime = sokol.time;
+const print = @import("std").debug.print;
+const shd = @import("3-attributes.glsl.zig");
+
+const state = struct {
+    var bind: sg.Bindings = .{};
+    var pip: sg.Pipeline = .{};
+    var pass_action: sg.PassAction = .{};
+};
+
+export fn init() void {
+    sg.setup(.{
+        .environment = sglue.environment(),
+        .logger = .{ .func = slog.func },
+    });
+
+    // initialize sokol_time
+    stime.stm_setup();
+
+    state.bind.vertex_buffers[0] = sg.makeBuffer(.{ .data = sg.asRange(&[_]f32{ 0.5, -0.5, 0.0, 1.0, 0.0, 0.0, -0.5, -0.5, 0, 0.0, 1.0, 0.0, 0.0, 0.5, 0.0, 0.0, 0.0, 1.0 }) });
+
+    var pip_desc: sg.PipelineDesc = .{ .shader = sg.makeShader(shd.simpleShaderDesc(sg.queryBackend())) };
+    pip_desc.layout.attrs[shd.ATTR_vs_position].format = .FLOAT3;
+    pip_desc.layout.attrs[shd.ATTR_vs_aColor].format = .FLOAT3;
+    state.pip = sg.makePipeline(pip_desc);
+
+    state.pass_action.colors[0] = .{
+        .load_action = .CLEAR,
+        .clear_value = .{ .r = 0.2, .g = 0.3, .b = 0.3, .a = 1 },
+    };
+
+    print("Backend {}\n", .{sg.queryBackend()});
+}
+
+export fn frame() void {
+    sg.beginPass(.{ .action = state.pass_action, .swapchain = sglue.swapchain() });
+    sg.applyPipeline(state.pip);
+    sg.applyBindings(state.bind);
+    sg.draw(0, 3, 1);
+    sg.endPass();
+    sg.commit();
+}
+
+export fn cleanup() void {
+    sg.shutdown();
+}
+
+export fn input(ev: ?*const sapp.Event) void {
+    const event = ev.?;
+    if (event.type == .KEY_DOWN) {
+        if (event.key_code == .ESCAPE) {
+            sapp.requestQuit();
+        }
+    }
+}
+
+pub fn main() void {
+    sapp.run(.{
+        .init_cb = init,
+        .frame_cb = frame,
+        .cleanup_cb = cleanup,
+        .event_cb = input,
+        .width = 640,
+        .height = 480,
+        .icon = .{ .sokol_default = true },
+        .window_title = "clear.zig",
+        .logger = .{ .func = slog.func },
+        .win32_console_attach = true,
+    });
+}
